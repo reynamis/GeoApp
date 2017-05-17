2b3d4bdfbfc58e1595394a8f2e2fdb5899faafe1
reynamis <12470545@itcampeche.edu.mx>
2017-05-17 18:06:02 -0500

    versio con interaccion con el usuario par a una sola pregunta

--- app/src/main/java/com/jorgechuc/android/geoapp/GeoActivity.java
+++ app/src/main/java/com/jorgechuc/android/geoapp/GeoActivity.java
@@ -2,12 +2,42 @@ package com.jorgechuc.android.geoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class GeoActivity extends AppCompatActivity {
+    Button mBotoncierto;
+    Button mBotonFalso;
+    TextView mTextPregunta;
+    Button mBotonSiguiente;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo);
+        mBotoncierto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoActivity.this, R.string.texto_correcto,Toast.LENGTH_SHORT).show();
+            }
+        });
+        mBotonFalso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoActivity.this, R.string.texto_incorrecto,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        
+
+
     }
-}
+
+
+    }
+
